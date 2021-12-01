e514173b2359dc02e21eb17a9440354a83993004
Marcin <marcinskalski1993@gmail.com>
2021-12-01 20:47:52 +0100

    Experimenting with overriding default repository behavior. Hiding repository methods through overriding.

--- src/main/java/learning/projects/todoapp/repository/TaskRepository.java
+++ src/main/java/learning/projects/todoapp/repository/TaskRepository.java
@@ -2,8 +2,25 @@ package learning.projects.todoapp.repository;
 
 import learning.projects.todoapp.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
+
 
 @RepositoryRestResource
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Long id);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Task task);
+
+    @RestResource(path = "done", rel = "done")
+    List<Task> findAllByDone(@Param("state") boolean done);
+
 }
