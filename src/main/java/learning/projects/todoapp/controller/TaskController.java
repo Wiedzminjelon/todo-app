97db1f33898e4e4ad6358724eb4b0b288c25796b
Marcin <marcinskalski1993@gmail.com>
2021-12-02 20:38:35 +0100

    Learning how to use Pageable interface.

--- src/main/java/learning/projects/todoapp/controller/TaskController.java
+++ src/main/java/learning/projects/todoapp/controller/TaskController.java
@@ -1,11 +1,15 @@
 package learning.projects.todoapp.controller;
 
+import learning.projects.todoapp.model.Task;
 import learning.projects.todoapp.repository.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 
 @RepositoryRestController
@@ -17,9 +21,15 @@ public class TaskController {
         this.taskRepository = taskRepository;
     }
 
-    @GetMapping(value = "/tasks")
-    public ResponseEntity<?> readAllTasks() {
+    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
+    public ResponseEntity<List<Task>> readAllTasks() {
         logger.info("Reading all tasks from repository");
         return ResponseEntity.ok(taskRepository.findAll());
+    }
+
+    @GetMapping(value = "/tasks")
+    public ResponseEntity<List<Task>> readAllTasks(Pageable pageable) {
+        logger.info("Custom pageable");
+        return ResponseEntity.ok(taskRepository.findAll(pageable).getContent());
     }
 }
